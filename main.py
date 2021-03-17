d2f789bcecee11ff1691efe05bfbaa346fb762e4
Andreas Tepper <tepper.andreas@gmail.com>
2021-03-17 19:17:05 +0100

    Servo-Steuerung eingebaut

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from threading import Thread
 from cam_stream import CamServer
 from ultrasonic import Ultrasonic
+from servo import Servo
 
 if __name__ == '__main__':
     try:
         Thread(target=CamServer().run_server, args=()).start()
         Thread(target=Ultrasonic().print_distance_to_console, args=()).start()
+        Thread(target=Servo().runServo(), args=()).start()
     except KeyboardInterrupt:
         print("Programm vom User gestoppt")
